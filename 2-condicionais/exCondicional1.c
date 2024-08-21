98be3ade43e73e270b5bfde63c66ec587652d3d3
Nathan Lima <nathanls.dev@gmail.com>
2024-08-21 16:23:40 -0300

    condicionais basicas 1

---
+++ 2-condicionais/exCondicional1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{
+  int num1, num2, soma;
+  printf("Informe o primeiro número inteiro: ");
+  scanf("%d", &num1);
+
+  printf("Informe o segundo número inteiro: ");
+  scanf("%d", &num2);
+
+  soma = num1 + num2;
+
+  if (soma >= 10)
+  {
+    printf("A soma de %d e %d é maior que 10: %d", num1, num2, soma);
+  }
+  else
+  {
+    printf("A soma de %d e %d não é maior que 10: %d", num1, num2, soma);
+  }
+}
