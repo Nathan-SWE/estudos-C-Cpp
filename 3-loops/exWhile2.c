85da44716fade5393ecf98b594e49d2f381fb073
Nathan Lima <nathanls.dev@gmail.com>
2024-08-22 12:55:46 -0300

    ex while loop 2

--- 3-loops/exWhile2.c
+++ 3-loops/exWhile2.c
@@ -4,7 +4,23 @@
 
 void main()
 {
-  // programa que imprime numeros pares, a partir de 10 até o número informado pelo usuário.
+  // programa que imprime números pares, a partir de 10 até o número informado pelo usuário.
   setlocale(LC_ALL, "pt-BR.UTF-8");
-  
+
+  int num, limit = 10;
+  printf("Informe um número para ver os valores pares de 10 até o valor desejado: ");
+  scanf("%d", &num);
+
+  while (limit <= num)
+  {
+    if (limit % 2 == 0)
+    {
+      printf("\n%d", limit);
+    }
+    limit++;
+  }
+
+  getchar();
+  printf("\nPressione enter para encerrar...");
+  getchar();
 }
