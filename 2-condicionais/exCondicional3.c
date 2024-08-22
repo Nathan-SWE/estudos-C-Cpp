34e48f231194cdf005814cc8432a3bd6b61f4adf
Nathan Lima <nathanls.dev@gmail.com>
2024-08-22 09:52:19 -0300

    ex condicionais 3

---
+++ 2-condicionais/exCondicional3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{
+  // programa que imprime se um número é par ou não;
+
+  int num;
+
+  printf("Informe um número inteiro para descobrir se ele é par: ");
+  scanf("%d", &num);
+
+  if (num % 2 == 0)
+  {
+    printf("O número %d é par", num);
+  }
+  else
+  {
+    printf("O número %d é impar", num);
+  }
+}
