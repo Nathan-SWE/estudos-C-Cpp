1e386e0b35c0627f2274465b1a81591c35c51581
Nathan Lima <nathanls.dev@gmail.com>
2024-08-22 11:20:36 -0300

    ex condicionais 9 - calc basica

---
+++ 2-condicionais/exCondicional9.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{
+  // programa de calculadora básica, leia dois números, mostre as opções de operações, e imprima o resultado.
+
+  float num1, num2, resultado;
+  int operacao;
+
+  printf("Escolha qual operação deseja realizar:");
+  printf("\n1 - Soma");
+  printf("\n2 - Subtração");
+  printf("\n3 - Multiplicação");
+  printf("\n4 - Divisão\n\n");
+  scanf("%d", &operacao);
+
+  switch (operacao)
+  {
+  case 1:
+    printf("Informe os números que deseja somar: ");
+    scanf("%f %f", &num1, &num2);
+    resultado = num1 + num2;
+    printf("%.2f + %.2f = %.2f", num1, num2, resultado);
+    break;
+  case 2:
+    printf("Informe os números que deseja subtrair: ");
+    scanf("%f %f", &num1, &num2);
+    resultado = num1 - num2;
+    printf("%.2f - %.2f = %.2f", num1, num2, resultado);
+    break;
+  case 3:
+    printf("Informe os números que deseja multiplicar: ");
+    scanf("%f %f", &num1, &num2);
+    resultado = num1 * num2;
+    printf("%.2f * %.2f = %.2f", num1, num2, resultado);
+    break;
+  case 4:
+    printf("Informe os números que deseja dividir: ");
+    scanf("%f %f", &num1, &num2);
+    resultado = num1 / num2;
+    printf("%.2f / %.2f = %.2f", num1, num2, resultado);
+    break;
+  default:
+    printf("Opção inválida, execute o programa novamente!");
+    break;
+  }
+}
