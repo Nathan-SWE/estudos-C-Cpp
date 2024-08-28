c14344ab53d7cb54e4453f35ec6304895daf0575
Nathan Lima <nathanls.dev@gmail.com>
2024-08-28 15:23:01 -0300

    ex Vetor 2

---
+++ 4-vetores-matrizes/exVetor2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+#define TAM_MOCHILA 5
+#define TAM_NOME 20
+
+void main()
+{
+  // inventário que permita ao usuário verificar o que há guardado, remover e adicionar itens.
+  setlocale(LC_ALL, "pt-BR.UTF-8");
+  int opcao, remover, sair = 0;
+  char mochila[TAM_MOCHILA][TAM_NOME] = {0};
+  char item[20] = {0};
+
+  while (sair != 1)
+  {
+    printf("\nO que gostaria de realizar ?\n");
+    printf("1 - Verificar mochila.\n");
+    printf("2 - Adicionar item à mochila.\n");
+    printf("3 - Remover item da mochila.\n");
+    printf("4 - Sair.\n");
+    scanf("\n%d", &opcao);
+
+    switch (opcao)
+    {
+    case 4:
+      printf("\nSaindo do sistema!");
+      sair = 1;
+      break;
+    default:
+      printf("\nValor inválido, tente novamente uma das opções!\n");
+      break;
+    }
+  }
+
+  getchar();
+  printf("\nPressione enter para encerrar...");
+  getchar();
+}
